2c697081bb2b2b02dec00ccfaec7ffd6f777b836
Towel2000 <72384225+Towel2000@users.noreply.github.com>
2024-01-31 12:33:59 +0800

    init

--- app.py
+++ app.py
@@ -24,7 +24,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    reply=message
+    if message.index('luck',0,3):
+        reply=message.strip(luck)
+    line_bot_api.reply_message(event.reply_token, reply)
 
 import os
 if __name__ == "__main__":
