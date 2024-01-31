186f9b739326756756ada5f75eedc38aeb786db9
Towel2000 <72384225+Towel2000@users.noreply.github.com>
2024-01-31 13:46:13 +0800

    init

--- app.py
+++ app.py
@@ -23,10 +23,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    if event.message.text.find('luck')==0:
-       reply=message.strip('luck')
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply))
+    message = text = event.message.text
+    if message.find('luck')==0:
+        line_bot_api.reply_message(event.reply_token,[TextSendMessage(message[4:])])
 
 import os
 if __name__ == "__main__":
