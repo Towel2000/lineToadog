fc8c2352fb355ae13cbe72a947eaf540e8c1bf24
Towel2000 <72384225+Towel2000@users.noreply.github.com>
2024-02-08 22:50:56 +0800

    init

--- app.py
+++ app.py
@@ -75,9 +75,8 @@ def handle_message(event):
                     if i>2:
                         line_bot_api.reply_message(event.reply_token,[TextSendMessage('Sorry, we can\'t roll more than 99 dices at a time ><')])
                     else:
-                        #line_bot_api.reply_message(event.reply_token,[TextSendMessage('assholoe')])
-                        for _ in i:
-                            luckynumber = luckynumber + str(random.randint(0,20)) + '\n'
+                        for bb in i:
+                            luckynumber = luckynumber + str(random.randint(0,20)) + '\n'   
                         line_bot_api.reply_message(event.reply_token,[TextSendMessage('as')])
                         no_leading_num_message = message[i+1:]
                         luckynumber = luckynumber + no_leading_num_message.strip()#error?w
