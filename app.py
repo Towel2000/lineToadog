699d2006e101e2e479f4deefdf64100c92959ad9
Towel2000 <72384225+Towel2000@users.noreply.github.com>
2024-02-09 13:31:58 +0800

    init

--- app.py
+++ app.py
@@ -65,13 +65,11 @@ def handle_message(event):
     message = text = event.message.text
 
     if message[0].isnumeric():
-        i = 0
         if message[1] =='d':
             luckynumber = ''
             for g in range(int(message[0])):
                 luckynumber = luckynumber + str(random.randint(0,20)) + '\n'
-            line_bot_api.reply_message(event.reply_token,[TextSendMessage('as')])
-            no_leading_num_message = message[i+1:]
+            no_leading_num_message = message[2:]
             luckynumber = luckynumber + no_leading_num_message.strip()
             line_bot_api.reply_message(event.reply_token,[TextSendMessage(luckynumber)])
     if message.find('command')==0:
